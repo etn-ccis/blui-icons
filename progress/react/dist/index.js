66759fa107b6b53f1e67e20dc2316cab21187fd7
joebochill <boyle.p.joseph@gmail.com>
2018-11-20 14:21:03 -0500

    Add ES6 to babel presets for react progress

--- progress/react/dist/index.js
+++ progress/react/dist/index.js
@@ -1,3 +1,29 @@
-export { Battery } from './Battery';
-export { Heart } from './Heart';
-export { Pie } from './Pie';
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Battery", {
+  enumerable: true,
+  get: function get() {
+    return _Battery.Battery;
+  }
+});
+Object.defineProperty(exports, "Heart", {
+  enumerable: true,
+  get: function get() {
+    return _Heart.Heart;
+  }
+});
+Object.defineProperty(exports, "Pie", {
+  enumerable: true,
+  get: function get() {
+    return _Pie.Pie;
+  }
+});
+
+var _Battery = require("./Battery");
+
+var _Heart = require("./Heart");
+
+var _Pie = require("./Pie");
