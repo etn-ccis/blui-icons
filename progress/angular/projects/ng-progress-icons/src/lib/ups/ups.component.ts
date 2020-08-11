7cc7de077c96545db4f42cf20829f791dc248c29
huayunh <hhuayun0828@gmail.com>
2020-08-10 21:33:24 -0400

    Update outlined progress icon design

--- progress/angular/projects/ng-progress-icons/src/lib/ups/ups.component.ts
+++ progress/angular/projects/ng-progress-icons/src/lib/ups/ups.component.ts
@@ -11,20 +11,22 @@ import { rangeValue } from '../utilities';
                 [attr.fill]="color || 'currentColor'"
                 *ngIf="!outlined"
             />
-            <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M22 2H2V22H22V2ZM4 4V7H6V8H4V12H6V13H4V17H6V18H4V20H20V18H18V17H20V13H18V12H20V8H18V7H20V4H4Z"
-                [attr.fill]="color || 'currentColor'"
-                *ngIf="outlined"
-            />
-            <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="2" y="2" width="20" height="20">
+            <mask
+                id="mask0"
+                mask-type="alpha"
+                maskUnits="userSpaceOnUse"
+                x="2"
+                y="2"
+                width="20"
+                height="20"
+                *ngIf="!outlined"
+            >
                 <path
                     d="M2 2H22V7H19V8H22V12H19V13H22V17H19V18H22V22H2V18H5V17H2V13H5V12H2V8H5V7H2V2Z"
                     [attr.fill]="color || 'currentColor'"
                 />
             </mask>
-            <g mask="url(#mask0)">
+            <g mask="url(#mask0)" *ngIf="!outlined">
                 <rect
                     x="2"
                     [attr.y]="startY"
@@ -33,6 +35,22 @@ import { rangeValue } from '../utilities';
                     [attr.fill]="color || 'currentColor'"
                 />
             </g>
+
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M22 2H2V22H22V2ZM4 4V8H6V9H4V12H6V13H4V16H6V17H4V20H20V17H18V16H20V13H18V12H20V9H18V8H20V4H4Z"
+                [attr.fill]="color || 'currentColor'"
+                *ngIf="outlined"
+            />
+            <rect
+                x="2"
+                [attr.y]="startY"
+                width="20"
+                [attr.height]="fillHeight"
+                [attr.fill]="color || 'currentColor'"
+                *ngIf="outlined"
+            />
         </svg>
     `,
     styles: [],
--- progress/react/src/Ups.tsx
+++ progress/react/src/Ups.tsx
@@ -12,26 +12,34 @@ export const Ups: React.FC<UpsProgressProps> = (props) => {
     return (
         <svg height={`${size}px`} width={`${size}px`} x={0} y={0} viewBox="0 0 24 24" {...svgProps}>
             {!outlined && (
-                <path
-                    opacity={0.3}
-                    d="M2 2H22V7H19V8H22V12H19V13H22V17H19V18H22V22H2V18H5V17H2V13H5V12H2V8H5V7H2V2Z"
-                    fill={color}
-                />
+                <>
+                    <path
+                        opacity={0.3}
+                        d="M2 2H22V7H19V8H22V12H19V13H22V17H19V18H22V22H2V18H5V17H2V13H5V12H2V8H5V7H2V2Z"
+                        fill={color}
+                    />
+                    <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x={2} y={2} width={20} height={20}>
+                        <path
+                            d="M2 2H22V7H19V8H22V12H19V13H22V17H19V18H22V22H2V18H5V17H2V13H5V12H2V8H5V7H2V2Z"
+                            fill={color}
+                        />
+                    </mask>
+                    <g mask="url(#mask0)">
+                        <rect x={2} y={startY} width={20} height={fillHeight} fill={color} />
+                    </g>
+                </>
             )}
             {outlined && (
-                <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M22 2H2V22H22V2ZM4 4V7H6V8H4V12H6V13H4V17H6V18H4V20H20V18H18V17H20V13H18V12H20V8H18V7H20V4H4Z"
-                    fill={color}
-                />
+                <>
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M22 2H2V22H22V2ZM4 4V8H6V9H4V12H6V13H4V16H6V17H4V20H20V17H18V16H20V13H18V12H20V9H18V8H20V4H4Z"
+                        fill={color}
+                    />
+                    <rect x={2} y={startY} width={20} height={fillHeight} fill={color} />
+                </>
             )}
-            <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x={2} y={2} width={20} height={20}>
-                <path d="M2 2H22V7H19V8H22V12H19V13H22V17H19V18H22V22H2V18H5V17H2V13H5V12H2V8H5V7H2V2Z" fill={color} />
-            </mask>
-            <g mask="url(#mask0)">
-                <rect x={2} y={startY} width={20} height={fillHeight} fill={color} />
-            </g>
         </svg>
     );
 };
