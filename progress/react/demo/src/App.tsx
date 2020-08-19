5b3670563c8d23fa31bcde64194ac0c5d40d6198
Firm-Soft <evan.w.mclaughlin@gmail.com>
2020-08-19 13:38:53 -0400

    Add react demo

--- progress/react/demo/src/App.tsx
+++ progress/react/demo/src/App.tsx
@@ -10,31 +10,151 @@ function App() {
               <Battery percent={10} size={36} color="#983fef"/>
               <Pie percent={10} size={36} color="#da7777" />
               <Ups percent={10} size={36} color="#0088f2"/>
-              <Heart percent={54} size={36}  color="#f0cb2f"/>
+              <Heart percent={54} size={36} color="#f0cb2f"/>
         </div>
 
-        <br />
+          <br />
+             <br />
           Center
+          <br />
           <div>
               24px
+              <br />
               <Battery percent={10} size={24} color="#983fef" showPercentLabel={true}/>
                 <Pie percent={10} size={24} color="#da7777" showPercentLabel={true} />
                 <Ups percent={10} size={24} color="#0088f2" showPercentLabel={true}/>
-                <Heart percent={54} size={24}  color="#f0cb2f" showPercentLabel={true}/>
+                <Heart percent={54} size={24} color="#f0cb2f" showPercentLabel={true}/>
         </div>
           <div>
               36px
+              <br />
               <Battery percent={10} size={36} color="#983fef" showPercentLabel={true}/>
               <Pie percent={10} size={36} color="#da7777" showPercentLabel={true} />
               <Ups percent={10} size={36} color="#0088f2" showPercentLabel={true}/>
-              <Heart percent={54} size={36}  color="#f0cb2f" showPercentLabel={true}/>
+              <Heart percent={54} size={36} color="#f0cb2f" showPercentLabel={true}/>
           </div>
           <div>
               48px
-              <Battery percent={10} size={48} color="#983fef" showPercentLabel={true}/>
-              <Pie percent={10} size={48} color="#da7777" showPercentLabel={true} />
-              <Ups percent={10} size={48} color="#0088f2" showPercentLabel={true}/>
-              <Heart percent={54} size={48}  color="#f0cb2f" showPercentLabel={true}/>
+              <br />
+              <Battery percent={10} size={48} color="#983fef" showPercentLabel={true} labelColor={'blue'}/>
+              <Pie percent={10} size={48} color="#da7777" showPercentLabel={true} labelColor={'red'} />
+              <Ups percent={10} size={48} color="#0088f2" showPercentLabel={true} labelColor={'yellow'}/>
+              <Heart percent={54} size={48} color="#f0cb2f" showPercentLabel={true} labelColor={'black'}/>
+          </div>
+
+          <br />
+          <br />
+          Left
+          <div>
+              24px
+              <br />
+              <Battery percent={10} size={24} color="#983fef" showPercentLabel={true} labelPosition={'left'}/>
+              <Pie percent={10} size={24} color="#da7777" showPercentLabel={true} labelPosition={'left'}/>
+              <Ups percent={10} size={24} color="#0088f2" showPercentLabel={true} labelPosition={'left'}/>
+              <Heart percent={54} size={24} color="#f0cb2f" showPercentLabel={true} labelPosition={'left'}/>
+          </div>
+          <div>
+              36px
+              <br />
+              <Battery percent={10} size={36} color="#983fef" showPercentLabel={true} labelPosition={'left'}/>
+              <Pie percent={10} size={36} color="#da7777" showPercentLabel={true} labelPosition={'left'}/>
+              <Ups percent={10} size={36} color="#0088f2" showPercentLabel={true} labelPosition={'left'}/>
+              <Heart percent={54} size={36} color="#f0cb2f" showPercentLabel={true} labelPosition={'left'}/>
+          </div>
+          <div>
+              48px
+              <br />
+              <Battery percent={10} size={48} color="#983fef" showPercentLabel={true} labelPosition={'left'}/>
+              <Pie percent={10} size={48} color="#da7777" showPercentLabel={true}  labelPosition={'left'}/>
+              <Ups percent={10} size={48} color="#0088f2" showPercentLabel={true} labelPosition={'left'}/>
+              <Heart percent={54} size={48}  color="#f0cb2f" showPercentLabel={true} labelPosition={'left'}/>
+          </div>
+
+
+          <br />
+          <br />
+          Right
+          <div>
+              24px
+              <br />
+              <Battery percent={10} size={24} color="#983fef" showPercentLabel={true} labelPosition={'right'}/>
+              <Pie percent={10} size={24} color="#da7777" showPercentLabel={true} labelPosition={'right'}/>
+              <Ups percent={10} size={24} color="#0088f2" showPercentLabel={true} labelPosition={'right'}/>
+              <Heart percent={54} size={24} color="#f0cb2f" showPercentLabel={true} labelPosition={'right'}/>
+          </div>
+          <div>
+              36px
+              <br />
+              <Battery percent={10} size={36} color="#983fef" showPercentLabel={true} labelPosition={'right'}/>
+              <Pie percent={10} size={36} color="#da7777" showPercentLabel={true} labelPosition={'right'}/>
+              <Ups percent={10} size={36} color="#0088f2" showPercentLabel={true} labelPosition={'right'}/>
+              <Heart percent={54} size={36} color="#f0cb2f" showPercentLabel={true} labelPosition={'right'}/>
+          </div>
+          <div>
+              48px
+              <br />
+              <Battery percent={10} size={48} color="#983fef" showPercentLabel={true} labelPosition={'right'}/>
+              <Pie percent={10} size={48} color="#da7777" showPercentLabel={true} labelPosition={'right'} />
+              <Ups percent={10} size={48} color="#0088f2" showPercentLabel={true} labelPosition={'right'}/>
+              <Heart percent={54} size={48} color="#f0cb2f" showPercentLabel={true} labelPosition={'right'}/>
+          </div>
+
+
+          <br />
+          <br />
+          Top
+          <div>
+              24px
+              <br />
+              <Battery percent={10} size={24} color="#983fef" showPercentLabel={true} labelPosition={'top'}/>
+              <Pie percent={10} size={24} color="#da7777" showPercentLabel={true} labelPosition={'top'} />
+              <Ups percent={10} size={24} color="#0088f2" showPercentLabel={true} labelPosition={'top'}/>
+              <Heart percent={54} size={24} color="#f0cb2f" showPercentLabel={true} labelPosition={'top'}/>
+          </div>
+          <div>
+              36px
+              <br />
+              <Battery percent={10} size={36} color="#983fef" showPercentLabel={true} labelPosition={'top'}/>
+              <Pie percent={10} size={36} color="#da7777" showPercentLabel={true} labelPosition={'top'} />
+              <Ups percent={10} size={36} color="#0088f2" showPercentLabel={true} labelPosition={'top'}/>
+              <Heart percent={54} size={36} color="#f0cb2f" showPercentLabel={true} labelPosition={'top'}/>
+          </div>
+          <div>
+              48px
+              <br />
+              <Battery percent={10} size={48} color="#983fef" showPercentLabel={true} labelPosition={'top'}/>
+              <Pie percent={10} size={48} color="#da7777" showPercentLabel={true} labelPosition={'top'} />
+              <Ups percent={10} size={48} color="#0088f2" showPercentLabel={true} labelPosition={'top'}/>
+              <Heart percent={54} size={48} color="#f0cb2f" showPercentLabel={true} labelPosition={'top'}/>
+          </div>
+
+
+          <br />
+          <br />
+          Bottom
+          <div>
+              24px
+              <br />
+              <Battery percent={10} size={24} color="#983fef" showPercentLabel={true} labelPosition={'bottom'}/>
+              <Pie percent={10} size={24} color="#da7777" showPercentLabel={true} labelPosition={'bottom'}/>
+              <Ups percent={10} size={24} color="#0088f2" showPercentLabel={true} labelPosition={'bottom'}/>
+              <Heart percent={54} size={24} color="#f0cb2f" showPercentLabel={true} labelPosition={'bottom'}/>
+          </div>
+          <div>
+              36px
+              <br />
+              <Battery percent={10} size={36} color="#983fef" showPercentLabel={true} labelPosition={'bottom'}/>
+              <Pie percent={10} size={36} color="#da7777" showPercentLabel={true} labelPosition={'bottom'}/>
+              <Ups percent={10} size={36} color="#0088f2" showPercentLabel={true} labelPosition={'bottom'}/>
+              <Heart percent={54} size={36} color="#f0cb2f" showPercentLabel={true} labelPosition={'bottom'}/>
+          </div>
+          <div>
+              48px
+              <br />
+              <Battery percent={10} size={48} color="#983fef" showPercentLabel={true}  labelPosition={'bottom'}/>
+              <Pie percent={10} size={48} color="#da7777" showPercentLabel={true} labelPosition={'bottom'}/>
+              <Ups percent={10} size={48} color="#0088f2" showPercentLabel={true}  labelPosition={'bottom'}/>
+              <Heart percent={54} size={48} color="#f0cb2f" showPercentLabel={true} labelPosition={'bottom'}/>
           </div>
         </>
   );
--- progress/react/icons/src/ProgressIcon.tsx
+++ progress/react/icons/src/ProgressIcon.tsx
@@ -1,32 +1,41 @@
 import React from 'react';
 import {ProgressIconProps} from "./types";
-import { makeStyles, createStyles } from '@material-ui/core/styles';
+import {makeStyles, createStyles, Theme} from '@material-ui/core/styles';
+import clsx from 'clsx';
+import color from 'color';
 
-
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles<Theme, ProgressIconProps>(() =>
     createStyles({
         progressIcon: {
             position: 'relative',
             justifyContent: 'center',
             alignItems: 'center',
-            display: 'inline-block',
         },
         progressIconLabel: {
+            display: 'flex',
             textAlign: 'center',
             zIndex: 2,
-            '&.center': {
-                fontWeight: 900,
-                webkitTextFillColor: 'black',
-                webkitTextStrokeColor: 'white'
-            }
+            fontSize: (props): number => props.labelSize || props.size/4,
+            WebkitTextStrokeWidth: (props): number => props.size/60,
+            color: (props): string => props.labelColor,
         },
+        centered: {
+            width: (props): number => props.size,
+            height: (props): number => props.size,
+            lineHeight: (props): string => `${props.size}px`,
+            fontWeight: 900,
+            display: 'block',
+            position: 'absolute',
+            '-webkitTextFillColor': (props): string => props.labelColor,
+            '-webkitTextStrokeColor': (props): string => color && color(props.labelColor).isDark() ? 'white' : 'black',
+            '-webkitTextStrokeWidth': (props): string => `${props.size / 60}px`
+        }
     })
 );
 
 export const ProgressIcon: React.FC<ProgressIconProps> = (props) => {
-
-    const { showPercentLabel, size = 24, labelPosition, labelColor, labelSize, children, percent } = props;
-    const classes = useStyles();
+    const { showPercentLabel, labelPosition = 'center', children, percent } = props;
+    const defaultClasses = useStyles(props);
 
     const getFlexDirection = (): any => {
         switch (labelPosition) {
@@ -47,21 +56,10 @@ export const ProgressIcon: React.FC<ProgressIconProps> = (props) => {
     const isCentered = (): boolean => labelPosition === 'center';
 
     return (
-        <span className={classes.progressIcon} style={{ flexDirection: getFlexDirection()}}>
+        <span className={defaultClasses.progressIcon} style={{ display: isCentered() ? 'inline-block' : 'inline-flex', flexDirection: getFlexDirection()}}>
             {showPercentLabel &&
-                <span
-                    className={classes.progressIconLabel}
-                    style={
-                    {   color: labelColor,
-                        width: isCentered() ? labelSize : 'unset',
-                        height: isCentered() ? labelSize : 'unset',
-                        WebkitTextStrokeWidth: size/60,
-                        lineHeight: (labelPosition === 'top' || labelPosition === 'bottom' ? 'unset' : `${size}px`),
-                        position: isCentered() ? 'absolute' : 'unset',
-                        display: isCentered() ? '' : 'flex'
-                    }
-                }>
-                    {percent}
+                <span className={clsx(defaultClasses.progressIconLabel, isCentered() ? defaultClasses.centered : '')}>
+                    {percent}%
                 </span>
             }
             {children}
