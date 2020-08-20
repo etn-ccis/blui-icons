36de5ccb54c6ba3d4edd30ccf0dc5cc17638f44e
huayunh <hhuayun0828@gmail.com>
2020-08-20 15:19:58 -0400

    Looks perfectly fine to me locally; no clue what's happening

--- progress/angular/demos/showcase/src/app/app.component.spec.ts
+++ progress/angular/demos/showcase/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NgProgressIconsModule } from '@pxblue/ng-progress-icons';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -7,6 +8,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        NgProgressIconsModule
+      ]
     }).compileComponents();
   }));
 
