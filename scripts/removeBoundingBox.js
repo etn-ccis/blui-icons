77cf9b48bb96a5213ab6313a4dfdb1a1cc9b6b70
huayunh <hhuayun0828@gmail.com>
2020-09-11 15:28:57 -0400

    And the script to remove all the boxes

---
+++ scripts/removeBoundingBox.js
@@ -0,0 +1,34 @@
+// from the root folder, run `node ./scripts/removeBoundingBox.js`
+
+const folder = './design/';
+const fs = require('fs');
+const path = require('path');
+
+async function* walk(dir) {
+    for await (const d of await fs.promises.opendir(dir)) {
+        const entry = path.join(dir, d.name);
+        if (d.isDirectory()) yield* walk(entry);
+        else if (d.isFile()) yield entry;
+    }
+}
+
+async function main() {
+    for await (const doc of walk(folder)) {
+        if (doc.slice(doc.length - 4) === '.svg') {
+            const data = fs.readFileSync(doc, { encoding: 'utf-8', flag: 'r' });
+            const sanitizedData = data
+                .replace(/<rect width="24" height="24" fill="none"\/>/g, '')
+                .replace(
+                    /<rect width="24" height="24" fill="none" stroke="#000" stroke-width="0" opacity="0.01"\/>/g,
+                    ''
+                )
+                .replace(
+                    /<rect width="24" height="24" transform="translate\(24 24\) rotate\(-180\)" fill="none"\/>/g,
+                    ''
+                );
+            fs.writeFileSync(doc, sanitizedData, { encoding: 'utf-8', flag: 'w' });
+        }
+    }
+    console.log('Success!');
+}
+main();
