79cbfa281c413aac2deef7015b61c8cb9a628a5c
joebochill <boyle.p.joseph@gmail.com>
2021-11-15 10:28:58 -0500

    Update mui demo to use latest brightlayer packages

--- mui/demos/showcase/.eslintrc.js
+++ mui/demos/showcase/.eslintrc.js
@@ -1,11 +1,11 @@
-module.exports =  {
-        root: true,
-        parser:  '@typescript-eslint/parser',
-        extends:  [ '@pxblue/eslint-config/tsx' ],
-        parserOptions:  {
-            project: "./tsconfig.json",
-        },
-        env: {
-            browser: true
-        }
-    };
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    extends: ['@brightlayer-ui/eslint-config/tsx'],
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    env: {
+        browser: true,
+    },
+};
