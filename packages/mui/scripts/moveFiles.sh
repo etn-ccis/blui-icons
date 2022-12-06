1bf0be1e2cba8715b50c723fa7f584f11593d8fc
joebochill <boyle.p.joseph@gmail.com>
2022-12-06 16:33:41 -0500

    Fix rename script extensions

--- packages/mui/scripts/moveFiles.sh
+++
@@ -1,5 +0,0 @@
-# This script renames the two files below to follow our naming convention for capitalization
-# The renaming in the component definitions and the index file happen through the SVGR icon and index templates
-[ ! -f "./tsx/EvPlugIec62196T3A.jsx" ] || mv ./tsx/EvPlugIec62196T3A.jsx ./tsx/EvPlugIec62196T3a.jsx
-[ ! -f "./tsx/EvPlugIec62196T3C.jsx" ] || mv ./tsx/EvPlugIec62196T3C.jsx ./tsx/EvPlugIec62196T3c.jsx
-exit 0
