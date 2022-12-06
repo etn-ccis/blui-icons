33f9d8930bfe2ec7b1ab0a622a3bc0fd7cba599d
joebochill <boyle.p.joseph@gmail.com>
2022-12-06 15:47:26 -0500

    Try renaming files before compiling

--- packages/mui/scripts/moveFiles.sh
+++ packages/mui/scripts/moveFiles.sh
@@ -1,5 +1,5 @@
 # This script renames the two files below to follow our naming convention for capitalization
 # The renaming in the component definitions and the index file happen through the SVGR icon and index templates
-[ ! -f "./dist/EvPlugIec62196T3A.js" ] || mv ./dist/EvPlugIec62196T3A.js ./dist/EvPlugIec62196T3a.js
-[ ! -f "./dist/EvPlugIec62196T3C.js" ] || mv ./dist/EvPlugIec62196T3C.js ./dist/EvPlugIec62196T3c.js
+[ ! -f "./tsx/EvPlugIec62196T3A.jsx" ] || mv ./tsx/EvPlugIec62196T3A.jsx ./tsx/EvPlugIec62196T3a.jsx
+[ ! -f "./tsx/EvPlugIec62196T3C.jsx" ] || mv ./tsx/EvPlugIec62196T3C.jsx ./tsx/EvPlugIec62196T3c.jsx
 exit 0
