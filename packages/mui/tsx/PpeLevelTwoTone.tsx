fefa37a6739245dadaaa1a5d25977c817e514374
huayunh <hhuayun0828@gmail.com>
2023-02-24 12:55:14 -0500

    Fix inline colors

--- packages/mui/tsx/PpeLevelTwoTone.tsx
+++ packages/mui/tsx/PpeLevelTwoTone.tsx
@@ -5,7 +5,13 @@ const SvgPpeLevelTwoTone = createSvgIcon(
         React.Fragment,
         {},
         <path fillRule="evenodd" clipRule="evenodd" d="M12 2 1 21h22L12 2Z" />,
-        <path opacity={0.7} fillRule="evenodd" clipRule="evenodd" d="M10 8h3l-2 5 4-2-2 6h1l-2 3-2-3h1l1-3-3 2 1-8Z" />
+        <path
+            opacity={0.7}
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M10 8h3l-2 5 4-2-2 6h1l-2 3-2-3h1l1-3-3 2 1-8Z"
+            fill="black"
+        />
     ),
     'SvgPpeLevelTwoTone'
 );
