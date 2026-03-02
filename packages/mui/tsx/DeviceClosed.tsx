7e3deb3d835b2354fcdcdbc646a86df2f3989c2d
Arshdeep Singh <arshdeepsingh3@eaton.com>
2026-03-02 12:40:09 +0530

    resolve merge conflicts

---
+++ packages/mui/tsx/DeviceClosed.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createSvgIcon } from '@mui/material/utils';
+const SvgDeviceClosed = createSvgIcon(
+    React.createElement(
+        React.Fragment,
+        {},
+        <path
+            clipRule="evenodd"
+            d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14Zm-8 3v12h2V6h-2Z"
+        />
+    ),
+    'SvgDeviceClosed'
+);
+export default SvgDeviceClosed;
---
+++ packages/mui/tsx/DeviceOpenOutline.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createSvgIcon } from '@mui/material/utils';
+const SvgDeviceOpenOutline = createSvgIcon(
+    React.createElement(
+        React.Fragment,
+        {},
+        <path d="M19 19v2H5v-2h14Zm0-14H5v16l-.204-.01a2 2 0 0 1-1.785-1.786L3 19V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v14a2 2 0 0 1-1.796 1.99L19 21V5Z" />,
+        <path clipRule="evenodd" d="M12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10Zm0 2a3 3 0 1 0 0 6 3 3 0 0 0 0-6Z" />
+    ),
+    'SvgDeviceOpenOutline'
+);
+export default SvgDeviceOpenOutline;
