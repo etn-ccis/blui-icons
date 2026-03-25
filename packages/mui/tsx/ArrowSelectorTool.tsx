dacf0b59ff5de4c1784a412637503241c285364f
manojleaton <manojlokesh@eaton.com>
2026-03-25 13:14:08 +0530

    build updates

--- packages/mui/tsx/ArrowSelectorTool.tsx
+++ packages/mui/tsx/ArrowSelectorTool.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { createSvgIcon } from '@mui/material/utils';
 const SvgArrowSelectorTool = createSvgIcon(
-    React.createElement(
-        React.Fragment,
-        {},
-        <path d="m13.775 22-3.625-7.8L6 20V2l14 11h-7.1l3.6 7.725L13.775 22Z" fill="#000" />
-    ),
+    React.createElement(React.Fragment, {}, <path d="m13.775 22-3.625-7.8L6 20V2l14 11h-7.1l3.6 7.725L13.775 22Z" />),
     'SvgArrowSelectorTool'
 );
 export default SvgArrowSelectorTool;
--- packages/mui/tsx/Section.tsx
+++ packages/mui/tsx/Section.tsx
@@ -8,7 +8,6 @@ const SvgSection = createSvgIcon(
             fillRule="evenodd"
             clipRule="evenodd"
             d="M18 4c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H6c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2h12Zm-5 5a1 1 0 0 1-1 1H6v8h12V6h-5v3ZM6 8h5V6H6v2Z"
-            fill="#000"
         />
     ),
     'SvgSection'
